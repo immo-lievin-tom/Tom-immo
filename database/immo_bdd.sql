3df6ab88baad0b0af7dc45b574c6528661845a9f
dufour olivier <olivier.dufour62@yahoo.com>
2020-02-20 11:22:12 +0100

    modif olivier

--- database/immo_bdd.sql
+++ database/immo_bdd.sql
@@ -4,23 +4,6 @@ DROP DATABASE IF EXISTS bdd_immo;
 CREATE DATABASE bdd_immo;
 USE bdd_immo;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-    `id` int(10) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `firstname` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `date_birth` DATETIME NOT NULL, 
-    `date_create` DATETIME DEFAULT CURRENT_DATE,
-    `date_update` DATETIME DEFAULT CURRENT_DATE,
-    `date_delete` DATETIME DEFAULT CURRENT_DATE,
-    `role` varchar DEFAULT 1,
-    `id_address` int, 
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`id_adress`) references address(id)
-)ENGINE=innoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
 DROP TABLE IF EXISTS `property`;
 CREATE TABLE `property` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -37,15 +20,14 @@ CREATE TABLE `property` (
     `isActive` BOOLEAN,
     `isTop` BOOLEAN,
     `isVIsible` BOOLEAN,
-    `date_create` DATETIME DEFAULT CURRENT_DATE,
-    `date_update` DATETIME DEFAULT CURRENT_DATE,
-    `date_delete` DATETIME DEFAULT CURRENT_DATE,
-    `id_user` INT,
-    `id_cat` INT,
-    `id_address` INT,
-    FOREIGN KEY(`id_user`) REFERENCES users(id), 
-    FOREIGN KEY(`id_cat`) REFERENCES category(id), 
-    FOREIGN KEY(`id_address`) REFERENCES address(id)
+    `role` VARCHAR DEFAULT 1,
+    `id_address` int, 
+    `id_user` int, 
+    `id_cat` int, 
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`id_user`) REFERENCES user(id),
+    FOREIGN KEY (`id_address`) REFERENCES address(id),
+    FOREIGN KEY (`id_cat`) REFERENCES category(id)
 )ENGINE=innoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 COMMIT;
