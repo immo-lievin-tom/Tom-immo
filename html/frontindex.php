13533d8004d40a113556d0aa621c0652438dafd7
Laslaa <mohamed.laslaa@gmail.com>
2020-02-21 11:57:09 +0100

    add front header

---
+++ html/frontindex.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <header class="headercontainer">
+        <?php require_once('header.html') ?>
+    </header>
+
+    <main>
+
+
+
+    </main>
+
+    <footer>
+
+    </footer>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+   <script src="https://kit.fontawesome.com/1de34be0f9.js" crossorigin="anonymous"></script> 
+</body>
+
+</html>
