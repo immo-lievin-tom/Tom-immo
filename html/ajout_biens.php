5bfe39da6eeff4c331244850e9f670c22ada7450
dufour olivier <olivier.dufour62@yahoo.com>
2020-02-25 12:38:40 +0100

    property finis

--- html/ajout_biens.php
+++ html/ajout_biens.php
@@ -23,7 +23,7 @@
             ?>
             <!-- <div class="col-10 pl-0 pt-5"> -->
             <div class="col-12 col-sm-8 mx-auto col-lg-5 col-xl-9 mt-4 border p-2 mx-2 property-espace">
-                <h4 class="text-center">Proposer un bien</h4>
+                <h4 class="text-center">Ajouter un bien</h4>
                 <form method="post" class="text-center mx-auto">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input m-2" type="radio" id="sale" name="contrat" value="sale">
@@ -116,8 +116,8 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="../js/admin.js"></script>
-    <script src="../js/espaceform.js"></script>
+    <script src="js/admin.js"></script>
+    
 </body>
 
 </html>
--- html/biens.php
+++ html/biens.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/icofont/icofont.min.css">
     <link rel="stylesheet" href="css/theo.css">
     <title>Document</title>
 </head>
@@ -25,87 +26,148 @@
                 <table class="table table-striped mx-auto border w-100">
                     <thead>
                         <tr>
-                            <th scope="col">Titre</th>
-                            <th scope="col">Référence</th>
-                            <th scope="col">Prix</th>
-                            <th scope="col">Modifier</th>
-                            <th scope="col">Top</th>
-                            <th scope="col">Visible</th>
-                            <th scope="col">Supprimer</th>
+                            <th class="text-center" scope="col" >Titre</th>
+                            <th class="text-center" scope="col" >Référence</th>
+                            <th class="text-center" scope="col" >Prix</th>
+                            <th class="text-center" scope="col" >Modifier</th>
+                            <th class="text-center" scope="col" >Top</th>
+                            <th class="text-center" scope="col" >Visible</th>
+                            <th class="text-center" scope="col">Supprimer</th>
 
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center" class="text-center">1524362</td>
+                            <td class="text-center">15€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop">
+                                    <label class="custom-control-label" for="customSwitchesTop"></label>
+                                </div>
+                            </td>
 
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
-
-                        </tr>
-                        <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
-
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
-
-                        </tr>
-                        <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
-
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesVisible">
+                                    <label class="custom-control-label" for="customSwitchesVisible"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
 
                         </tr>
                         <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center">1524362</td>
+                            <td class="text-center">15€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop1">
+                                    <label class="custom-control-label" for="customSwitchesTop1"></label>
+                                </div>
+                            </td>
 
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesVisible1">
+                                    <label class="custom-control-label" for="customSwitchesVisible1"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
 
                         </tr>
                         <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center">1524362</td>
+                            <td class="text-center">15€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop2">
+                                    <label class="custom-control-label" for="customSwitchesTop2"></label>
+                                </div>
+                            </td>
 
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesVisible2">
+                                    <label class="custom-control-label" for="customSwitchesVisible2"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
 
                         </tr>
                         <tr>
-                            <td scope="row">Maison</td>
-                            <td>1524362</td>
-                            <td>15€</td>
-                            <td><button>Modifier</button></td>
-                            <td><button>lol</button></td>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center">1524362</td>
+                            <td class="text-center">15€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop3">
+                                    <label class="custom-control-label" for="customSwitchesTop3"></label>
+                                </div>
+                            </td>
 
-                            <td><button>lol</button></td>
-                            <td><button>Supprimer</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesVisible3">
+                                    <label class="custom-control-label" for="customSwitchesVisible3"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
+
+                        </tr>
+                        <tr>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center">1524362</td>
+                            <td class="text-center">15€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop4">
+                                    <label class="custom-control-label" for="customSwitchesTop4"></label>
+                                </div>
+                            </td>
+
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesVisible4">
+                                    <label class="custom-control-label" for="customSwitchesVisible4"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
+
+                        </tr>
+                        <tr>
+                            <td class="text-center" scope="row">Maison</td>
+                            <td class="text-center">1524362</td>
+                            <td class="text-center">150000000000€</td>
+                            <td class="text-center"><button class="btn bg-white mx-auto col-12 rounded-0 btndelete">Modifier</button></td>
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="customSwitchesTop5">
+                                    <label class="custom-control-label" for="customSwitchesTop5"></label>
+                                </div>
+                            </td>
+
+                            <td class="text-center">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input coucou" id="customSwitchesVisible5">
+                                    <label class="custom-control-label coucou" for="customSwitchesVisible5"></label>
+                                </div>
+                            </td>
+                            <td class="text-center" class="text-center"><a class="text-decoration-none text-dark" href="#"><i class="icofont-ui-delete icofont-2x"></i></a></td>
 
                         </tr>
                     </tbody>
                 </table>
                 <div class="col-12 d-flex justify-content-end bouton_ajout">
-                    <a href="ajout_biens.php"><button>Ajouter</button></a>
+                    <a href="ajout_biens.php"><button type="submit" class="btn btn-outline-dark color1b0a2e text-white">Ajouter</button></a>
+
                 </div>
             </div>
 
