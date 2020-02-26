5461cd1bc47801af4dd998b37b86ce6227cb8a5e
dufour olivier <olivier.dufour62@yahoo.com>
2020-02-26 08:39:06 +0100

    href ok

--- config/config.php
+++ config/config.php
@@ -4,13 +4,12 @@ define('ROOT', __DIR__ . '/');
 
 define('BASE_DIR', '/tom-immo/public/');
 define('BASE_ADMIN', '/tom-immo/public/admin');
+define('BASE_ESPACE', '/tom-immo/public/espace/myaccount/');
 define('BASE_JS', '/tom-immo/public/assets/js/');
 define('BASE_CSS', '/tom-immo/public/assets/css/');
 define('BASE_IMG', '/tom-immo/public/assets/img/');
 define('PATH_VIEWS', ROOT . '../app/views/');
 define('PATH_MODELS', ROOT . '../app/models/');
-define('PATH_ESPACE', '/tom-immo/public/espace/myaccount/');
-define('PATH_ADMIN', '/tom-immo/public/espace/myaccount/');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'bdd_immo');
 define('DB_USER', 'root');
