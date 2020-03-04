33059b6150610c4bffe7a751698f8a75a44a2714
Devise <t.devise@laposte.net>
2020-03-04 11:04:18 +0100

    messages finished

--- app/controllers/admin/IndexController.php
+++ app/controllers/admin/IndexController.php
@@ -5,6 +5,9 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\Admin\AppController;
 use App\Models\Message;
+use League\Csv\Writer;
+use App\Models\Property;
+
 
 class IndexController extends AppController
 {
@@ -29,26 +32,27 @@ class IndexController extends AppController
         $this->render('index/listuser');
     }
 
-    public function listmessageAction($id='', $action='')
+    public function listmessageAction($id = '', $action = '')
     {
         $message = new Message();
-       
-        if($id!='' && $action == 'delete'){
+
+        if ($id != '' && $action == 'delete') {
             $message->setId($id);
-            $message->updateIs(['isActive' => 0 ]);
+            $message->updateIs(['isActive' => 0]);
         }
-        $array['all'] = $message->selectBy( ['isActive' => 1]);
+        $array['all'] = $message->selectBy(['isActive' => 1]);
         $this->render('index/listmessage', $array);
     }
 
     public function detailmessageAction($id)
-    {   $message = new Message();
+    {
+        $message = new Message();
         $message->setId($id);
         $array['one'] = $message->select();
 
-        $message->updateIs(['isRead' => 1 ]);
+        $message->updateIs(['isRead' => 1]);
 
-        $this->render('index/detail_message',$array);
+        $this->render('index/detail_message', $array);
     }
     public function addpropertyAction()
     {
@@ -70,6 +74,30 @@ class IndexController extends AppController
     }
     public function exportAction()
     {
+
+        $properties = new Property();
+        $allProperties['all'] = $properties->selectPerso(['id', 'name', 'reference', 'price', 'isActive', 'isTop', 'isVisible']);
+
+        if (isset($_POST['valider'])){
+            //we create the CSV into memory
+        $csv = Writer::createFromFileObject(new \SplTempFileObject());
+
+        //we insert the CSV header
+        $csv->insertOne(['firstname', 'lastname', 'email']);
+
+        // The PDOStatement Object implements the Traversable Interface
+        // that's why Writer::insertAll can directly insert
+        // the data into the CSV
+        $csv->insertAll($allProperties['all']);
+
+        // Because you are providing the filename you don't have to
+        // set the HTTP headers Writer::output can
+        // directly set them for you
+        // The file is downloadable
+        $csv->output('users.csv');
+        die;
+        }
+        
         $this->render('index/export');
     }
 }
--- app/views/admin/index/export.php
+++ app/views/admin/index/export.php
@@ -1,7 +1,7 @@
 
 <div class="mx-auto col-10 row">
     <h4 class="text-center my-3 col-12">Exporter les biens</h4>
-    <form action="" class="mx-auto col-4 text-center">
+    <form action="" method="POST" class="mx-auto col-4 text-center">
         <div class="pl-0 pt-5 d-flex justify-content-around">
             <div class="text-center">
                 <label class="label" for="start">Date de début:</label>
@@ -11,6 +11,6 @@
                 <input class="input" type="date" id="end" name="trip-end">
             </div>
         </div>
-        <button type="button" class="btn btn-info my-5">Valider</button>
+        <button type="submit" name="valider" class="btn btn-info my-5">Valider</button>
     </form>
 </div>
