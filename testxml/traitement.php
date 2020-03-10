3691589008f8f59f236eb3ac634b7bef041e11d0
Laslaa <mohamed.laslaa@gmail.com>
2020-03-10 09:17:31 +0100

    testxmlfinish

--- testxml/traitement.php
+++ testxml/traitement.php
@@ -6,7 +6,8 @@ use App\Models\Property;
 $dbman = new Property();
 
 $dom = new DOMDocument('1.0', 'utf-8');
-
+$contain = $dom->createElement('contain');
+$dom->appendChild($contain);
 $result = $dbman->selectSpecific("SELECT * FROM `property` WHERE date_create BETWEEN '" .  $_POST['datebegin'] . "' and '" . $_POST['dateend'] . "'");
 
 var_dump($result);
@@ -15,10 +16,10 @@ foreach ($result as $key) {
     foreach ($key as $key2 => $value) {
         if ($key2 == 'date_create') {
             $element = $dom->createElement('datecreate', $value);
-            $dom->appendChild($element);
+            $contain->appendChild($element);
         } elseif ($key2 == 'id') {
             $element = $dom->createElement('path', "/property/$value");
-            $dom->appendChild($element);
+            $contain->appendChild($element);
         }
     }
 }
